6ea6ce5da5b8d0d1e2ce4883d54312d9817cafad
nareshBedadala <NareshBedadala@ritchiebros.com>
2023-12-21 15:17:06 +0530

    Initial Commit

---
+++ Sample-iOS/Sample-iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Sample-iOS
+//
+//  Created by Naresh kumar B on 21/12/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
